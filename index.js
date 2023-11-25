cf750ba38568c047ee5b565b4a72ea1bd0a5bead
andrew <andrew_hoffman262@outlook.com>
2023-11-24 23:30:14 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+class Polygon {
+    constructor(array) {
+    this.array = array;
+    }
+    get countSides() {
+        return this.array.length
+    }
+    get perimeter() {
+        return this.array.reduce((total, side) => total + side, 0);
+    }
+}
+class Triangle extends Polygon {
+    get isValid() {
+        if (this.array.length === 3) {
+            return (
+                this.array[0] + this.array[1] > this.array[2] &&
+                this.array[1] + this.array[2] > this.array[0] &&
+                this.array[0] + this.array[2] > this.array[1]
+              );
+            
+        } else {
+            return false
+        }
+    }
+}
+class Square extends Polygon {
+    get isValid() {
+        const sideLength = this.array[0];
+        return this.array.every(side => side === sideLength);
+        }
+    get area() {
+        const sideLength = this.array[0];
+        return sideLength ** 2
+    }
+}
